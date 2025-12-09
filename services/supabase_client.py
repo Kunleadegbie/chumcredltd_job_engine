bf7797eed078fd384a9ccb936f2899130ba9a253
Adekunle Adegbie <kadegbie@gmail.com>
2025-12-09 18:25:11 +0100

    Add clean settings.py without secrets

--- services/supabase_client.py
+++ services/supabase_client.py
@@ -1,6 +1,8 @@
 import os
-print("DEBUG SUPABASE_URL =", os.getenv("SUPABASE_URL"))
-print("DEBUG SUPABASE_KEY =", os.getenv("SUPABASE_KEY"))
+
+print("DEBUG: SUPABASE_URL =", repr(os.getenv("SUPABASE_URL")))
+print("DEBUG: SUPABASE_KEY =", repr(os.getenv("SUPABASE_KEY")))
+print("DEBUG: OPENAI_API_KEY =", repr(os.getenv("OPENAI_API_KEY")))
 
 from supabase import create_client
 import streamlit as st
